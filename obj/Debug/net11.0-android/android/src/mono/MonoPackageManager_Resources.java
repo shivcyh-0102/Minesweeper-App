889422eb171a64f6a22e2c362c2327a17f9806f5
Jm Gapas <gapas644@gmail.com>
2026-05-11 02:41:06 +0800

    Remove bin and obj folders

--- obj/Debug/net11.0-android/android/src/mono/MonoPackageManager_Resources.java
+++
@@ -1,7 +0,0 @@
-package mono;
-public class MonoPackageManager_Resources {
-	public static String[] Assemblies = new String[]{
-		/* "MinesweeperApp.dll" should be the only entry in this list. */
-		"MinesweeperApp.dll",
-	};
-}
